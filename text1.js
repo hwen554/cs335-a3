f737a7ecf7a7889983de87741041b8a66a7914bc
Hong Weng <wengh7930@gmail.com>
2022-11-11 16:58:26 +1300

    wix

---
+++ text1.js
@@ -0,0 +1,9 @@
+async function Home(){
+  document.getElementById("home").style.display = "block";
+  document.getElementById("login").style.display = "none";
+  document.getElementById("game").style.display = "none";
+  document.getElementById("shop").style.display = "none";
+  document.getElementById("register").style.display = "none";
+  document.getElementById("book").style.display = "none";
+  
+}
